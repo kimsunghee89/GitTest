65635c9a4c1359c11d18dd494666331173c71754
USER <USER@USER18-PC>
2017-01-17 10:05:52 +0900

    Add minus function

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -3,8 +3,13 @@ public class Calculator {
 
 	public int add(int num1,int num2){
 		int result=0;
-		result=num1;
+		result=num1+num2;
 		
 		return result;
+	}
+	public int minus( int num1,int num2){
+		int result1=0;
+		result1=num1-num2;
+		return result1;
 	}
 }
