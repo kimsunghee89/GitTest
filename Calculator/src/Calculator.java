cff5b1f0a37d1a4b7a982e746f9348938106e8ac
USER <USER@USER18-PC>
2017-01-18 10:47:47 +0900

    Update Program

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -9,7 +9,17 @@ public class Calculator {
 	}
 	public int sub( int num1,int num2){
 		int result1=0;
+		result1=num1-num2;
+		return result1;
+	}
+	public int mul( int num1,int num2){
+		int result1=0;
 		result1=num1*num2;
+		return result1;
+	}
+	public int div( int num1,int num2){
+		int result1=0;
+		result1=num1/num2;
 		return result1;
 	}
 }
---
+++ Calculator/src/Controller.java
@@ -0,0 +1,13 @@
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	Calculator cal = new Calculator();
+	
+	cal.add(1,2);
+	
+	}
+
+}
